04f73a89a54987000b5417e5a65b4ba6744db649
BMRX <mike.b.bergstrom@gmail.com>
2017-03-08 11:49:32 -0800

    lol

--- modules/command-handler.js
+++ modules/command-handler.js
@@ -6,21 +6,18 @@ module.exports = function (client, from, to, text, message) {
     var opts = {
         command: String(text.split(' ')[1]),
         argument: text.substring(String(text.split(' ')[0]).length).trim(),
-        messageToSend: ''
     }
 
-    console.log(opts);
-
     function externalCommand(opts) {
-            if (fs.existsSync('./commands/' + opts.command + '.js')) { // check if we have an command file
-                var output = require('../commands/' + opts.command + '.js')(client, from, to, text, message);
-                if (output) {
-                    client.say(to, output);
-                }
-            } else {
-                console.log("Not a command");
-            }
-    };
+    if (fs.existsSync('../commands/' + opts.command + '.js')) { // check if we have an command file
+      var output = require('../commands/' + opts.command + '.js')(client, from, to, text, message);
+      if (output) {
+        client.say(to, output);
+      }
+    } else {
+      client.say(to, "nope");
+    }
+  };
 
     externalCommand(opts);
 
